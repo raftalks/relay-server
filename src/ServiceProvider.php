25e11a9ff37e52193ca2a0b71f87a2b795c2ae26
Raf <7909000@gmail.com>
2017-11-11 17:55:26 +0500

    starting...

---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raf
+ * Date: 11/11/2017
+ * Time: 5:31 PM
+ */
+
+namespace Raftalks\RelayService;
+
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
